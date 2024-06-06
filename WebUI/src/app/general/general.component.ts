b188bfe2426149441a2dc33ded2a5d20432be2d6
ProjectHW <ya.ko4erapbuhunyx@yandex.ru>
2024-06-07 02:09:04 +0300

    add general component

--- WebUI/src/app/general/general.component.ts
+++ WebUI/src/app/general/general.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-general',
   templateUrl: './general.component.html',
   styleUrl: './general.component.css'
 })
-export class GeneralComponent {
+export class GeneralComponent implements OnInit {
+  private _url = 'ping'
 
+  constructor(
+    private _http: HttpClient,
+  ) {}
+
+  ngOnInit(): void {
+    this.get().subscribe((x: any) => {
+      console.log(x);
+    },
+  error => {
+    console.log(error);
+  });
+  }
+
+  get(): Observable<any> {
+    let test = this._http.get<any>(environment.urlAddress + this._url);
+
+    return test;
+  }
 }
